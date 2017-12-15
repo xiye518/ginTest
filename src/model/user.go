a9c1c98451245725c717cf5bccadab70afced4b2
tanrongxian <tanrongxian@jubao56.com>
2017-12-15 15:58:20 +0800

    add interface and wget test demo,change sql setting

--- src/model/user.go
+++ src/model/user.go
@@ -35,7 +35,7 @@ type Token struct {
 
 func init() {
 	var err error
-	db, err = sql.Open("mysql", "root:123456@tcp(localhost:3306)/gorm?charset=utf8")
+	db, err = sql.Open("mysql", "root:123456@tcp(localhost:3306)/gorm?charset=utf8&parseTime=True&loc=Local")
 	//db, err = sql.Open("mysql", "jubao:jubao@tcp(localhost:3306)/gorm?charset=utf8")
 	if err != nil {
 		panic(err.Error())
